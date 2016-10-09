ffa5f522a21b1544e6e2b803ffb2991b5b5edd24
jimboca <jimboca3@gmail.com>
2016-10-08 17:09:13 -0700

    #13 Full working version of FoscamHD2 camera

--- camera_funcs.py
+++ camera_funcs.py
@@ -16,3 +16,38 @@ def ip2long(ip):
 
 def long2ip(value):
     return socket.inet_ntoa(struct.pack('!L', value))
+
+
+# isBit() returns True or False if bit at offset is set or not
+def isBit(int_type, offset):
+    mask = 1 << offset
+    if (int_type & mask) == 0:
+        return False
+    return True
+
+# isBit() returns 1 or 0 if bit at offset is set or not
+def isBitI(int_type, offset):
+    mask = 1 << offset
+    if (int_type & mask) == 0:
+        return 0
+    return 1
+
+# testBit() returns a nonzero result, 2**offset, if the bit at 'offset' is one.
+def testBit(int_type, offset):
+    mask = 1 << offset
+    return(int_type & mask)
+
+# setBit() returns an integer with the bit at 'offset' set to 1.
+def setBit(int_type, offset):
+    mask = 1 << offset
+    return(int_type | mask)
+
+# clearBit() returns an integer with the bit at 'offset' cleared.
+def clearBit(int_type, offset):
+    mask = ~(1 << offset)
+    return(int_type & mask)
+
+# toggleBit() returns an integer with the bit at 'offset' inverted, 0 -> 1 and 1 -> 0.
+def toggleBit(int_type, offset):
+    mask = 1 << offset
+    return(int_type ^ mask)
